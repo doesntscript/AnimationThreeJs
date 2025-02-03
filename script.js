c7875f58b0a36f0f48fa364c5599da8980108fe4
Guilherme Cosme dos Santos <doesntscript@gmail.com>
2025-02-03 09:26:29 -0300

    (AuthPags): Learning about Animation in React Three.js, Animation function, Clock class, and the use of GetElapsedTime to achieve identical frames across different devices.

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 
+
 // Scene Mesh Camera Renderer
 
 // Scene
 const scene = new THREE.Scene();
+
+// Mesh
+
+const geometry = new THREE.BoxGeometry(1, 1, 1); 
+const material = new THREE.MeshBasicMaterial({color: "pink"});
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Camera 
+
+const aspect = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera( 75, aspect.width / aspect.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".draw"); // Select the canvas
+const renderer = new THREE.WebGLRenderer({ canvas }); // add WebGL Renderer
+renderer.setSize( aspect.width, aspect.height); // Renderer Size
+
+// Clock Class
+const clock = new THREE.Clock();
+
+// Animation 
+const animate = () => {
+    // GetElapsedTime
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update Rotation On X Axis
+    mesh.rotation.y = elapsedTime * Math.PI * 0.20;
+
+    // Renderer
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(animate);
+};
+animate();
